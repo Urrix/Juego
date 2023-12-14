72d354eb49182df48c72f7cb64ed2ce17daf06b8
Urrix <mauribarrien@gmail.com>
2023-12-14 12:08:49 -0600

    view

--- app/src/main/java/com/example/juego/MainActivity.java
+++ app/src/main/java/com/example/juego/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this,PlayerFragment.class);
+        Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
 
     }
