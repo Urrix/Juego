7a6f6f2a8bf692c90bb3fd61d5f5a03c5a1140a6
Urrix <mauribarrien@gmail.com>
2023-12-18 16:29:02 -0600

    Correcion

--- app/src/main/java/com/example/juego/PlayerFragment.java
+++ app/src/main/java/com/example/juego/PlayerFragment.java
@@ -10,11 +10,7 @@ import android.widget.Toast;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
-<<<<<<< HEAD
 
-=======
-import androidx.lifecycle.ViewModelProvider;
->>>>>>> feature/ViewModel
 
 public class PlayerFragment extends Fragment {
 
@@ -35,7 +31,7 @@ public class PlayerFragment extends Fragment {
 
         LiveData<Integer> touchCountLiveData;
         if (playerId == 1) {
-            AtouchCountLiveData = viewModel.getPlayer1TouchCount();
+            touchCountLiveData = viewModel.getPlayer1TouchCount();
         } else {
             touchCountLiveData = viewModel.getPlayer2TouchCount();
         }
@@ -44,24 +40,12 @@ public class PlayerFragment extends Fragment {
             @Override
             public void onChanged(Integer count) {
                 touchCountTextView.setText("Player " + playerId + " Count: " + count);
-<<<<<<< HEAD
 
                 if (count >= 10) {
                     touchCountTextView.setText("Player " + playerId + " wins!");
                     view.setOnTouchListener(null);
                     showCongratulationsToast(playerId);
                 }
-=======
-            }
-        });
-
-        viewModel.getGameOverState().observe(getViewLifecycleOwner(), new Observer<MainViewModel.GameOverState>() {
-            @Override
-            public void onChanged(MainViewModel.GameOverState gameOverState) {
-                int winner = gameOverState.getWinner();
-                int result = gameOverState.getResult();
-                showGameOverMessage(winner, result);
->>>>>>> feature/ViewModel
             }
         });
 
@@ -80,16 +64,10 @@ public class PlayerFragment extends Fragment {
         return view;
     }
 
-<<<<<<< HEAD
 
 
     private void showCongratulationsToast(int playerId) {
         String message = "¡Felicidades, Jugador " + playerId + "!";
         Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
-=======
-    private void showGameOverMessage(int winner, int result) {
-        String message = "Game Over!\nPlayer " + winner + " wins with a score of " + result;
-        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show();
->>>>>>> feature/ViewModel
     }
 }
